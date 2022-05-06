d31b2be33874995811fe701016e24853691c706e
danz1337z <danielz@getacademy.no>
2022-05-06 11:15:15 +0200

    2

---
+++ Oppgaver/Oppgaver om main-metoden og args/Oppgaver om main-metoden og args/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OppgaverOmMainMetodenOgArgs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.WriteLine(args.Length);
+
+        }
+    }
+}
---
+++ Oppgaver/OppgaverIfElse/OppgaverIfElse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OppgaverIfElse
+{
+    internal class Program
+    {
+        static bool Blabla()
+        {
+            int value1 = 5;
+            int value2 = 6;
+
+            bool status = value1 == value2;
+
+            if (status)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        static void Main(string[] args)
+        {
+            //Console.Write("Write a number: ");
+            //var input1 = Console.ReadLine();
+            //Console.Write("Write another number: ");
+            //var input2 = Console.ReadLine();
+
+            //if (input1.Equals(input2))
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            Console.Write(Blabla());
+
+        }
+    }
+}
